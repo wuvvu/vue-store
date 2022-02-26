1d346b354ae1fd99d8a3c9926a34a9d835b44a80
lhb-web <1156518655@qq.com>
2022-02-27 00:00:52 +0800

    去除注释

--- src/main.js
+++ src/main.js
@@ -1,10 +1,3 @@
-/*
- * 入口文件
- * @Author: hai-27
- * @Date: 2020-02-07 16:23:00
- * @LastEditors: hai-27
- * @LastEditTime: 2020-03-04 23:38:41
- */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
